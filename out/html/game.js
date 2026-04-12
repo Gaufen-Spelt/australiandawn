bcc078b931624e728d11c9aff7898870c98c60dd
Gaufen Johannes <kesselfarben@gmail.com>
2026-04-12 10:56:07 +0500

    gv

--- out/html/game.js
+++ out/html/game.js
@@ -25,6 +25,14 @@
       ui.loadGame(url);
   };
 
+  window.showCredits = function() {
+    if (window.dendryUI.dendryEngine.state.sceneId.startsWith('credits')) {
+        window.dendryUI.dendryEngine.goToScene('backSpecialScene');
+    } else {
+        window.dendryUI.dendryEngine.goToScene('credits');
+    }
+  };
+
   window.showStats = function() {
     if (window.dendryUI.dendryEngine.state.sceneId.startsWith('library')) {
         window.dendryUI.dendryEngine.goToScene('backSpecialScene');
